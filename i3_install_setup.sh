f6a5febc50de102baff22e8d537fd4c0219e12c0
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-05-16 17:27:47 -0700

    Update i3_install_setup.sh

--- i3_install_setup.sh
+++ i3_install_setup.sh
@@ -2,3 +2,5 @@
 
 apt install i3 lightdm thunar keepassxc pulseaudio pipewire-audio-client-libraries ssh tor mpv cava ufw firefox-esr mousepad xfce4-terminal lxappearance picom -y
 
+systemctl --user start pulseaudio.service
+systemctl --user enable pulseaudio.service
