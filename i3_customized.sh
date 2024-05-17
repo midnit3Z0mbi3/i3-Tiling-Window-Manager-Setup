689ccafc42a03f1427aeee524a3f7939ebc8f80f
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-05-16 21:59:03 -0700

    Update i3_customized.sh

--- i3_customized.sh
+++ i3_customized.sh
@@ -3,9 +3,14 @@ cp config ~/.config/i3/
 mkdir ~/.config/i3status
 cp i3status.conf ~/.config/i3status/
 mkdir ~/.config/cava
-cp dracula.cava ~/.config/cava/config
 mkdir -p ~/.local/share/xfce4/terminal/colorschemes
 cd ~
+mkdir custom
+cd custom
 git clone https://github.com/dracula/xfce4-terminal.git
 cd xfce4-terminal
 cp Dracula.theme ~/.local/share/xfce4/terminal/colorschemes
+cd ~/custom
+git clone https://github.com/dracula/cava.git
+cd cava
+cp dracula.cava ~/.config/cava/config
