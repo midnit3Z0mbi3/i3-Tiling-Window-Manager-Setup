365f970414957abd31847ac00eaf66b807badd1a
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-05-17 08:04:28 -0700

    Update i3_install_setup.sh

--- i3_install_setup.sh
+++ i3_install_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt install i3 lightdm thunar keepassxc pulseaudio pipewire ssh tor mpv cava ufw firefox-esr mousepad xfce4-terminal lxappearance picom -y
+apt install i3 lightdm thunar thunar-archive-plugin zip keepassxc pulseaudio pipewire ssh tor mpv cava ufw firefox-esr mousepad xfce4-terminal lxappearance picom -y
 
